2d3362703602fb41e4eafd3757d78c33b3a17012
Poojapentela <117167745+Poojapentela@users.noreply.github.com>
2023-08-29 17:54:15 +0530

    Add files via upload

---
+++ code clause/img.js
@@ -0,0 +1,25 @@
+function left() {
+    document.getElementById("text").style.textAlign = "left";
+}
+  
+function center() {
+    document.getElementById("text").style.textAlign = "center";
+}
+  
+function right() {
+    document.getElementById("text").style.textAlign = "right";
+}
+function upper() {
+    document.getElementById("text").style.textTransform = "uppercase";
+}
+  
+function lower() {
+    document.getElementById("text").style.textTransform = "lowercase";
+}
+  
+function captal() {
+    document.getElementById("text").style.textTransform = "capitalize";
+}
+function Bold() {
+    document.getElementById("text").style.fontWeight = "bold";
+}
